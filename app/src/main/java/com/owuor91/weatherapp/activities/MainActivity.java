13ab1c4bab79a4b037d4f9799229a98211949cf4
John Owuor <owuor91@gmail.com>
2016-10-10 13:16:05 +0300

    add background image

--- app/src/main/java/com/owuor91/weatherapp/activities/MainActivity.java
+++ app/src/main/java/com/owuor91/weatherapp/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -30,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Nairobi Weather");
+        toolbar.getBackground().setAlpha(0);
+
 
         long geodatacount = SugarRecord.count(Geoname.class, null, null);
         if (geodatacount==0){
